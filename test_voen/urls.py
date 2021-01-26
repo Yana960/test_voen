07b6553cdac21c04922eb8b93b2a6598ca656fbc
yana960 <yana.shavel@bk.ru>
2021-01-26 16:24:11 +0300

    1

--- test_voen/urls.py
+++ test_voen/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import velo.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', velo.views.showmain, name='showmain'),
 ]
